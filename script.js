bd8affc429ec3ce79ccc089c0d20a6246ccd5586
Maximilian13 <maxim.myagchilo@gmail.com>
2022-10-24 00:28:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ tg.expand()
 tg.MainButton.textColor = "#FFFFFF"
 tg.MainButton.color = "#2cab37"
 
-Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    tg.sendData("Котик")
-})
+let btn = document.querySelector("#click")
+
+btn.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("Вы выбрали котика!")
+        tg.MainButton.show()
+    }
+}
 
